84afe5486cc29d9bebb7c62338ac80cd99dfe435
Chrispolanco <chrispolanco@sbcglobal.net>
2022-08-21 13:20:36 -0700

    added asset folder || modification styles

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar";
 
 
 const App = () => {
   return(
     <div>
-      <h1>Re-vents</h1>
-      <EventDashboard/>
+      <NavBar/> 
+      <Container className="main">
+        <EventDashboard/>
+      </Container>
     </div>
   );
 }
